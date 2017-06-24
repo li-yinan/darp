a4ec679af9a0bd018efeee235a38e87d4af0569f
liyinan <liyinan@baidu.com>
2017-06-25 00:42:12 +0800

    修改编译脚本和babel配置，把一些不该编译的排除掉，直接拷贝过去

--- script/build.sh
+++ script/build.sh
@@ -2,3 +2,5 @@ rm -rf ./lib
 babel src --out-dir ./lib
 cp -r src/reporter/assets lib/reporter/
 cp -r src/reporter/templates lib/reporter/
+mkdir -p lib/static/report
+cp -r src/static/live.js lib/static/
