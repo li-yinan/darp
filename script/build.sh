97e5f8814021c3d914daa674cb4739b2544b28f6
liyinan <biline@163.com>
2017-07-01 12:17:30 +0800

    拷贝sample文件

--- script/build.sh
+++ script/build.sh
@@ -4,3 +4,4 @@ cp -r src/reporter/assets lib/reporter/
 cp -r src/reporter/templates lib/reporter/
 mkdir -p lib/static/report
 cp -r src/static/live.js lib/static/
+cp -r src/conf/config.js.sample lib/conf/
